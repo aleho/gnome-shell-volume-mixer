7523251623e677d249b8f3d71a1ace8a0651d6d0
Alexander Hofbauer <alex@derhofbauer.at>
2014-12-03 17:26:43 +0100

    settings.js: Implement more datatypes

--- shell-volume-mixer@derhofbauer.at/settings.js
+++ shell-volume-mixer@derhofbauer.at/settings.js
@@ -1,7 +1,7 @@
 /**
  * Shell Volume Mixer
  *
- * Advanced mixer extension.
+ * Convenience class to wrap Gio.Settings.
  *
  * @author Alexander Hofbauer <alex@derhofbauer.at>
  */
@@ -13,6 +13,8 @@ const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const Lang = imports.lang;
+
+const Utils = Extension.imports.utils;
 
 const POS_MENU = 0;
 const POS_LEFT = 1;
@@ -45,7 +47,7 @@ const Settings = new Lang.Class({
      */
     _initSettings: function() {
         let schemaName = 'org.gnome.shell.extensions.shell-volume-mixer';
-        let schemaDir = Extension.dir.get_child('schemas').get_path();
+        let schemaDir = Utils.getExtensionPath('schemas');
 
         if (GLib.file_test(schemaDir + '/gschemas.compiled', GLib.FileTest.EXISTS)) {
             let schemaSource = Gio.SettingsSchemaSource.new_from_directory(
@@ -61,7 +63,9 @@ const Settings = new Lang.Class({
             throw 'Schema "%s" not found'.format(schemaName);
 
         } else {
-            return new Gio.Settings({ schema: schemaName });
+            return new Gio.Settings({
+                schema: schemaName
+            });
         }
     },
 
@@ -94,21 +98,64 @@ const Settings = new Lang.Class({
         }
     },
 
-
-    get_enum: function(key) {
-        return this._settings.get_enum(key);
-    },
-
-    set_enum: function(key, value) {
-        return this._settings.set_enum(key, value);
+    get settings() {
+        return this._settings;
     },
 
 
+    /**
+     * Retrieves the value of an 's' type key.
+     */
+    get_string: function(key) {
+        return this._settings.get_string(key);
+    },
+
+    /**
+     * Sets the value of an 's' type key.
+     */
+    set_string: function(key, value) {
+        return this._settings.set_string(key, value);
+    },
+
+    /**
+     * Retrieves the value of a 'b' type key.
+     */
     get_boolean: function(key) {
         return this._settings.get_boolean(key);
     },
 
+    /**
+     * Sets the value of a 'b' type key.
+     */
     set_boolean: function(key, value) {
         return this._settings.set_boolean(key, value);
+    },
+
+    /**
+     * Retrieves the value of an enum key.
+     */
+    get_enum: function(key) {
+        return this._settings.get_enum(key);
+    },
+
+    /**
+     * Set the value of an enum key.
+     */
+    set_enum: function(key, value) {
+        return this._settings.set_enum(key, value);
+    },
+
+    /**
+     * Retrieves the value of an array key.
+     */
+    get_array: function(key) {
+        return this._settings.get_strv(key);
+    },
+
+    /**
+     * Set the value of an array key.
+     */
+    set_array: function(key, value) {
+        return this._settings.set_strv(key, value);
     }
 });
