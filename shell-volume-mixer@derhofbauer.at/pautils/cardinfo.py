ae8445332ab6ee752a0b1e1ada0a80498f635586
Alexander Hofbauer <alex@derhofbauer.at>
2014-12-03 17:22:36 +0100

    Add a Python script to lookup card details

---
+++ shell-volume-mixer@derhofbauer.at/pautils/cardinfo.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+#
+# Usage: cardinfo.py [card_index] [card_name]
+#
+# Output is either a card object or an array of all cards available, depending
+# on whether a card index / name was passed or no parameters at all.
+#
+# This file is part of GNOME Shell Volume Mixer
+# Copyright (C) 2014 Alexander Hofbauer <alex@derhofbauer.at>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import sys
+
+import pa
+
+
+class PulseAudio():
+    _name = 'ShellVolumeMixer'
+    _cards = None
+    _error = {
+        'success': False,
+        'error': None,
+    }
+    _card_op_done = None
+    _pa_state = pa.CONTEXT_UNCONNECTED
+
+
+    def __init__(self):
+        self.pa_mainloop = pa.mainloop_new()
+        self.pa_mainloop_api = pa.mainloop_get_api(self.pa_mainloop)
+
+        self._context = pa.context_new(self.pa_mainloop_api, self._name)
+        self._context_notify_cb = pa.context_notify_cb_t(self.context_notify_cb)
+
+        pa.context_set_state_callback(self._context, self._context_notify_cb, None)
+
+
+    def __enter__(self):
+        pa.context_connect(self._context, None, 0, None)
+        return self
+
+
+    def __exit__(self, eType, eValue, eTrace):
+        pa.context_disconnect(self._context)
+        pa.context_unref(self._context)
+        pa.mainloop_free(self.pa_mainloop)
+
+
+    def get_card_info(self, index = None, name = None):
+        self._cards = {}
+        self._card_op_done = False
+        operation = None
+
+        loops = 1000
+        while loops > 0:
+            loops -= 1
+
+            if self._pa_state == pa.CONTEXT_FAILED:
+                self._cards = self._error
+                self._cards['error'] = 'context failed'
+                break
+
+            if self._pa_state == pa.CONTEXT_TERMINATED:
+                self._cards = self._error
+                self._cards['error'] = 'context terminated'
+                break
+
+            if self._card_op_done == True:
+                break
+
+            if self._pa_state == pa.CONTEXT_READY and not operation:
+                self._pa_card_info_cb = pa.card_info_cb_t(self.pa_card_info_cb)
+
+                if name:
+                    operation = pa.context_get_card_info_by_name(self._context,
+                            name, self._pa_card_info_cb, None)
+
+                elif index >= 0:
+                    operation = pa.context_get_card_info_by_index(self._context,
+                            index, self._pa_card_info_cb, None)
+
+                else:
+                    operation = pa.context_get_card_info_list(self._context,
+                            self._pa_card_info_cb, None)
+
+
+            pa.mainloop_iterate(self.pa_mainloop, 0, None)
+
+
+        if operation:
+            pa.operation_unref(operation)
+
+
+        return self._cards
+
+
+    def pa_card_info_cb(self, context, struct, cindex, user_data):
+        if not struct or not struct[0]:
+            return
+
+        pacard = struct[0]
+
+        try:
+            description = pa.proplist_gets(pacard.proplist, 'device.description');
+        except:
+            pass
+
+        if not description:
+            try:
+                description = pa.proplist_gets(pacard.proplist, 'alsa.card_name');
+            except:
+                pass
+
+        if not description:
+            description = pacard.name;
+
+
+        card = {
+            'index': pacard.index,
+            'name': pacard.name,
+            'description': description,
+            'active_profile': None,
+            'profiles': [
+            ],
+        }
+
+        if pacard.active_profile and pacard.active_profile[0]:
+            ap = pacard.active_profile[0]
+            card['active_profile'] = ap.name
+
+        for index in xrange(0, pacard.n_profiles):
+            if not pacard.profiles2[index] or not pacard.profiles2[index][0]:
+                continue
+            profile = pacard.profiles2[index][0]
+            card['profiles'].append({
+                'name': profile.name,
+                'description': profile.description,
+                'available': bool(profile.available),
+            })
+
+        self._cards[card['index']] = card
+        self._card_op_done = True
+
+
+    def context_notify_cb(self, context, userdata):
+        try:
+            self._pa_state = pa.context_get_state(context)
+
+        except Exception:
+            self._pa_state = pa.CONTEXT_FAILED
+
+
+
+index = name = None
+
+if len(sys.argv) > 1:
+    if sys.argv[1].isdigit():
+        index = int(sys.argv[1])
+    else:
+        name = sys.argv[1]
+
+
+with PulseAudio() as pulse:
+    card = pulse.get_card_info(index = index, name = name)
+#    print(json.dumps(card, indent = 4, separators = (',', ': ')))
+    print(json.dumps(card))
---
+++ shell-volume-mixer@derhofbauer.at/pautils/pa.py
@@ -0,0 +1,163 @@
+# This library was generated through introspection of
+#     /usr/include/pulse/introspect.h and
+#     /usr/include/pulse/mainloop.h.
+#     (E.g.:
+#         python /usr/share/pyshared/ctypeslib/h2xml.py /usr/include/pulse/introspect.h -o pa.xml
+#         python /usr/share/pyshared/ctypeslib/xml2py.py pa.xml -k f -l /usr/lib/libpulse.so -o pa.py
+#     )
+#
+# License according to the header files listed above:
+# GNU Lesser General Public License
+#
+# This file is part of GNOME Shell Volume Mixer
+# Copyright (C) 2014 Alexander Hofbauer <alex@derhofbauer.at>
+
+
+from ctypes import *
+
+try:
+    lib = CDLL('libpulse.so.0')
+except:
+    lib = CDLL('libpulse.so')
+
+STRING = c_char_p
+WSTRING = c_wchar_p
+uint32_t = c_uint32
+int64_t = c_int64
+
+class mainloop(Structure):
+    pass
+
+class mainloop_api(Structure):
+    pass
+
+class spawn_api(Structure):
+    pass
+
+class context(Structure):
+    pass
+
+class operation(Structure):
+    pass
+
+class proplist(Structure):
+    pass
+
+class card_profile_info(Structure):
+    _fields_ = [
+        ('name', STRING),
+        ('description', STRING),
+        ('n_sinks', uint32_t),
+        ('n_sources', uint32_t),
+        ('priority', uint32_t),
+    ]
+
+class card_profile_info2(Structure):
+    _fields_ = [
+        ('name', STRING),
+        ('description', STRING),
+        ('n_sinks', uint32_t),
+        ('n_sources', uint32_t),
+        ('priority', uint32_t),
+        ('available', c_int),
+    ]
+
+class card_port_info(Structure):
+    _fields_ = [
+        ('name', STRING),
+        ('description', STRING),
+        ('priority', uint32_t),
+        ('available', c_int),
+        ('direction', c_int),
+        ('n_profiles', uint32_t),
+        ('profiles', POINTER(POINTER(card_profile_info))),
+        ('proplist', POINTER(proplist)),
+        ('latency_offset', int64_t),
+        ('profiles2', POINTER(POINTER(card_profile_info2))),
+    ]
+
+class card_info(Structure):
+    _fields_ = [
+        ('index', uint32_t),
+        ('name', STRING),
+        ('owner_module', uint32_t),
+        ('driver', STRING),
+        ('n_profiles', uint32_t),
+        ('profiles', POINTER(card_profile_info)),
+        ('active_profile', POINTER(card_profile_info)),
+        ('proplist', POINTER(proplist)),
+        ('n_ports', uint32_t),
+        ('ports', POINTER(POINTER(card_port_info))),
+        ('profiles2', POINTER(POINTER(card_profile_info2))),
+        ('active_profile2', POINTER(card_profile_info2)),
+    ]
+
+mainloop_new = lib.pa_mainloop_new
+mainloop_new.restype = POINTER(mainloop)
+mainloop_new.argtypes = []
+mainloop_get_api = lib.pa_mainloop_get_api
+mainloop_get_api.restype = POINTER(mainloop_api)
+mainloop_get_api.argtypes = [POINTER(mainloop)]
+mainloop_iterate = lib.pa_mainloop_iterate
+mainloop_iterate.restype = c_int
+mainloop_iterate.argtypes = [POINTER(mainloop), c_int, POINTER(c_int)]
+mainloop_free = lib.pa_mainloop_free
+mainloop_free.restype = None
+mainloop_free.argtypes = [POINTER(mainloop)]
+
+context_flags = c_int  # enum
+context_flags_t = context_flags
+
+context_state = c_int  # enum
+context_state_t = context_state
+# values for enumeration 'context_state'
+CONTEXT_UNCONNECTED = 0
+CONTEXT_CONNECTING = 1
+CONTEXT_AUTHORIZING = 2
+CONTEXT_SETTING_NAME = 3
+CONTEXT_READY = 4
+CONTEXT_FAILED = 5
+CONTEXT_TERMINATED = 6
+
+context_new = lib.pa_context_new
+context_new.restype = POINTER(context)
+context_new.argtypes = [POINTER(mainloop_api), STRING]
+context_notify_cb_t = CFUNCTYPE(None, POINTER(context), c_void_p)
+context_set_state_callback = lib.pa_context_set_state_callback
+context_set_state_callback.restype = None
+context_set_state_callback.argtypes = [POINTER(context), context_notify_cb_t, c_void_p]
+context_connect = lib.pa_context_connect
+context_connect.restype = c_int
+context_connect.argtypes = [POINTER(context), STRING, context_flags_t, POINTER(spawn_api)]
+context_disconnect = lib.pa_context_disconnect
+context_disconnect.restype = None
+context_disconnect.argtypes = [POINTER(context)]
+context_unref = lib.pa_context_unref
+context_unref.restype = None
+context_unref.argtypes = [POINTER(context)]
+context_get_state = lib.pa_context_get_state
+context_get_state.restype = context_state_t
+context_get_state.argtypes = [POINTER(context)]
+
+operation_unref = lib.pa_operation_unref
+operation_unref.restype = None
+operation_unref.argtypes = [POINTER(operation)]
+
+card_info_cb_t = CFUNCTYPE(None, POINTER(context), POINTER(card_info), c_int, c_void_p)
+context_get_card_info_by_index = lib.pa_context_get_card_info_by_index
+context_get_card_info_by_index.restype = POINTER(operation)
+context_get_card_info_by_index.argtypes = [POINTER(context), uint32_t, card_info_cb_t, c_void_p]
+context_get_card_info_by_name = lib.pa_context_get_card_info_by_name
+context_get_card_info_by_name.restype = POINTER(operation)
+context_get_card_info_by_name.argtypes = [POINTER(context), STRING, card_info_cb_t, c_void_p]
+context_get_card_info_list = lib.pa_context_get_card_info_list
+context_get_card_info_list.restype = POINTER(operation)
+context_get_card_info_list.argtypes = [POINTER(context), card_info_cb_t, c_void_p]
+
+proplist_gets = lib.pa_proplist_gets
+proplist_gets.restype = c_char_p
+proplist_gets.argtypes = [POINTER(proplist), c_char_p]
+
+proplist_to_string = lib.pa_proplist_to_string
+proplist_to_string.restype = c_char_p
+proplist_to_string.argtypes = [POINTER(proplist)]
