7d24e058964151f63873fe5236a4d6c3a50fa5e4
Alexander Hofbauer <alex@derhofbauer.at>
2020-03-22 23:32:55 +0100

    Refactor extension.js
    Move into it's own class, try to fix double-icon bug.

--- shell-volume-mixer@derhofbauer.at/extension.js
+++ shell-volume-mixer@derhofbauer.at/extension.js
@@ -10,117 +10,19 @@
 /* exported init, enable, disable */
 
 const Lib = imports.misc.extensionUtils.getCurrentExtension().imports.lib;
-const Main = imports.ui.main;
-const PopupMenu = imports.ui.popupMenu;
 
-const { Indicator } = Lib.menu.indicator;
-const { Mixer } = Lib.volume.mixer;
-const { PanelButton } = Lib.widget.panelButton;
-const Settings = Lib.settings;
+const { Extension } = Lib.main;
 
-let settings;
-
-let aggregateMenu;
-let volumeIndicator;
-let volumeIcon;
-let volumeMenu;
-let mixer;
-
-let menu;
-let gvmIndicator;
+let extension;
 
 function init() {
-    settings = new Settings.Settings();
-    aggregateMenu = Main.panel.statusArea.aggregateMenu;
-    volumeIndicator = aggregateMenu._volume;
-    volumeIcon = volumeIndicator._primaryIndicator;
-    volumeMenu = volumeIndicator._volumeMenu;
+    extension = new Extension();
 }
 
 function enable() {
-    settings.connectChanged(() => {
-        disable();
-        enable();
-    });
-
-    mixer = new Mixer();
-
-    let position = settings.get_enum('position');
-
-    if (position === Settings.POS_MENU) {
-        replaceOriginal();
-    } else {
-        addPanelButton(position);
-    }
-}
-
-function replaceOriginal() {
-    gvmIndicator = new Indicator(mixer, {
-        separator: false
-    });
-
-    volumeMenu.actor.hide();
-    volumeIcon.hide();
-
-    // get current indicator position
-    let indicators = aggregateMenu._indicators.get_children();
-    let indicatorPos = 4;
-    for (let i = 0; i < indicators.length; i++) {
-        if (volumeIndicator == indicators[i]) {
-            indicatorPos = i;
-            break;
-        }
-    }
-
-    // add our own indicator and menu
-    aggregateMenu._volume = gvmIndicator;
-    aggregateMenu._indicators.insert_child_at_index(gvmIndicator, indicatorPos);
-    aggregateMenu.menu.addMenuItem(gvmIndicator.menu, 0);
-
-    aggregateMenu.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem(), 1);
-
-    // on disable/enable we won't get a stream-changed event, so trigger it here to be safe
-    gvmIndicator.updateIcon();
-}
-
-function addPanelButton(position) {
-    let removeOriginal = settings.get_boolean('remove-original');
-    if (removeOriginal) {
-        volumeMenu.actor.hide();
-        volumeIcon.hide();
-    }
-
-    menu = new PanelButton(mixer);
-
-    if (position === Settings.POS_LEFT) {
-        Main.panel.addToStatusArea('ShellVolumeMenu', menu, 999, 'left');
-    } else if (position === Settings.POS_CENTER) {
-        Main.panel.addToStatusArea('ShellVolumeMenu', menu, 999, 'center');
-    } else {
-        Main.panel.addToStatusArea('ShellVolumeMenu', menu);
-    }
+    extension.enable();
 }
 
 function disable() {
-    volumeMenu.actor.show();
-    volumeIcon.show();
-    aggregateMenu._volume = volumeIndicator;
-
-    if (mixer) {
-        mixer.disconnectAll();
-        mixer = null;
-    }
-
-    if (gvmIndicator) {
-        aggregateMenu._indicators.remove_actor(gvmIndicator);
-        gvmIndicator.destroy();
-        gvmIndicator = null;
-    }
-
-    if (menu) {
-        menu.destroy();
-        menu = null;
-    }
-
-    Settings.cleanup();
+    extension.disable();
 }
---
+++ shell-volume-mixer@derhofbauer.at/lib/main.js
@@ -0,0 +1,171 @@
+/**
+ * Shell Volume Mixer
+ *
+ * Main extension setup.
+ *
+ * @author Alexander Hofbauer <alex@derhofbauer.at>
+ */
+
+/* exported Extension */
+
+const Lib = imports.misc.extensionUtils.getCurrentExtension().imports.lib;
+const Main = imports.ui.main;
+const PopupMenu = imports.ui.popupMenu;
+
+const { Mixer } = Lib.volume.mixer;
+const { Indicator } = Lib.menu.indicator;
+const { PanelButton } = Lib.widget.panelButton;
+const Settings = Lib.settings;
+
+const DEFAULT_INDICATOR_POS = 4;
+
+
+var Extension = class {
+    constructor() {
+        // save the original volume reference in aggregate menu.
+        this._orgVolume = this._menu._volume;
+    }
+
+    /**
+     * Settings instance.
+     * @private
+     */
+    get _settings() {
+        if (!this._settingsInstance) {
+            this._settingsInstance = new Settings.Settings();
+        }
+
+        return this._settingsInstance;
+    }
+
+    /**
+     * The shell aggregate menu instance.
+     * @private
+     */
+    get _menu() {
+        return Main.panel.statusArea.aggregateMenu;
+    }
+
+    enable() {
+        this._settings.connectChanged(() => {
+            this._disable();
+            this._enable();
+        });
+
+        this._mixer = new Mixer();
+
+        let position = this._settings.get_enum('position');
+
+        if (position === Settings.POS_MENU) {
+            this._replaceOriginal();
+        } else {
+            this._addPanelButton(position);
+        }
+    }
+
+    disable() {
+        this._menu._volume = this._orgVolume;
+        this._showOriginal();
+
+        if (this._mixer) {
+            this._mixer.disconnectAll();
+            this._mixer = null;
+        }
+
+        if (this._indicator) {
+            this._menu._indicators.remove_actor(this._indicator);
+            this._indicator.destroy();
+            this._indicator = null;
+        }
+
+        if (this._panelButton) {
+            this._panelButton.destroy();
+            this._panelButton = null;
+        }
+
+        Settings.cleanup();
+    }
+
+    /**
+     * Hides the original menu item and icon.
+     * @private
+     */
+    _hideOriginal() {
+        this._orgVolume._volumeMenu.actor.hide();
+        this._orgVolume._primaryIndicator.hide();
+        this._menu._indicators.remove_child(this._orgVolume);
+    }
+
+    /**
+     * Restores the original menu item and icon.
+     * @private
+     */
+    _showOriginal() {
+        this._menu._indicators.insert_child_at_index(this._orgVolume, this._indicatorPos || DEFAULT_INDICATOR_POS);
+        this._orgVolume._volumeMenu.actor.show();
+        this._orgVolume._primaryIndicator.show();
+    }
+
+    /**
+     * Replaces the current indicator and menu.
+     * @private
+     */
+    _replaceOriginal() {
+        this._indicator = new Indicator(this._mixer, {
+            separator: false
+        });
+
+        // get current indicator position
+        this._indicatorPos = this._getCurrentIndicatorPosition();
+        this._hideOriginal();
+
+        // add our own indicator and menu
+        this._menu._volume = this._indicator;
+        this._menu._indicators.insert_child_at_index(this._indicator, this._indicatorPos);
+        this._menu.menu.addMenuItem(this._indicator.menu, 0);
+
+        this._menu.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem(), 1);
+
+        // on disable/enable we won't get a stream-changed event, so trigger it here to be safe
+        this._indicator.updateIcon();
+    }
+
+    /**
+     * Find the current volume icon's position.
+     * @private
+     */
+    _getCurrentIndicatorPosition() {
+        let indicators = this._menu._indicators.get_children();
+        let indicatorPos = DEFAULT_INDICATOR_POS;
+
+        for (let i = 0; i < indicators.length; i++) {
+            if (this._orgVolume == indicators[i]) {
+                indicatorPos = i;
+                break;
+            }
+        }
+
+        return indicatorPos;
+    }
+
+    /**
+     * Inits this extension as stand-alone button.
+     * @private
+     */
+    _addPanelButton(position) {
+        let removeOriginal = this._settings.get_boolean('remove-original');
+        if (removeOriginal) {
+            this._hideOriginal();
+        }
+
+        this._panelButton = new PanelButton(this._mixer);
+
+        if (position === Settings.POS_LEFT) {
+            Main.panel.addToStatusArea('ShellVolumeMenu', this._panelButton, 999, 'left');
+        } else if (position === Settings.POS_CENTER) {
+            Main.panel.addToStatusArea('ShellVolumeMenu', this._panelButton, 999, 'center');
+        } else {
+            Main.panel.addToStatusArea('ShellVolumeMenu', this._panelButton);
+        }
+    }
+};
