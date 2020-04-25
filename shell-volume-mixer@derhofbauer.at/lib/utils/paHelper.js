cddf4824634b2965648e755f8f0d5f9eed3d3c52
Alexander Hofbauer <alex@derhofbauer.at>
2020-04-25 13:04:53 +0200

    Improve python detection
    Iterating in a loop doesn't make sense at all, we have to determine the
    valid Python executable before trying to call the helper script, and
    stop early.

--- shell-volume-mixer@derhofbauer.at/lib/utils/paHelper.js
+++ shell-volume-mixer@derhofbauer.at/lib/utils/paHelper.js
@@ -15,6 +15,33 @@ const Utils = Lib.utils.utils;
 
 const PYTHON_HELPER_PATH = 'pautils/cardinfo.py';
 
+let PYTHON = undefined;
+
+async function findPython() {
+    if (PYTHON === undefined) {
+        for (let python of ['python3', 'python']) {
+            let ret;
+            let stderr;
+
+            try {
+                [ret, , stderr] = await Process.execAsync(['/usr/bin/env', python]);
+
+                if (ret === 0) {
+                    PYTHON = python;
+                    break;
+                }
+
+                Log.error('paHelper', 'findPython', `${python} not found: ${stderr} (${ret})`);
+
+            } catch (e) {
+                PYTHON = false;
+                Log.error('paHelper', 'findPython', e);
+            }
+        }
+    }
+
+    return PYTHON;
+}
 
 /**
  * @param {?number} [index=undefined]
@@ -24,33 +51,38 @@ async function execHelper(index = undefined) {
     const paUtilPath = Utils.getExtensionPath(PYTHON_HELPER_PATH);
 
     if (!paUtilPath) {
-        Log.error('paHelper', 'getCards', `Could not find PulseAudio utility in extension path ${PYTHON_HELPER_PATH}`);
+        Log.error('paHelper', 'execHelper', `Could not find PulseAudio utility in extension path ${PYTHON_HELPER_PATH}`);
         return null;
     }
+
+    const python = await findPython();
+
+    if (!python) {
+        return null;
+    }
+
+    const args = ['/usr/bin/env', python, paUtilPath];
+
+    if (!isNaN(index)) {
+        args.push(index);
+    }
+
 
     let ret;
     let stdout;
     let stderr;
     let pythonError;
 
-    for (let python of ['python3', 'python']) {
-        const args = ['/usr/bin/env', python, paUtilPath];
-
-        if (!isNaN(index)) {
-            args.push(index);
-        }
-
-        try {
-            [ret, stdout, stderr] = await Process.execAsync(args);
-        } catch (e) {
-            pythonError = e;
-        }
+    try {
+        [ret, stdout, stderr] = await Process.execAsync(args);
+    } catch (e) {
+        pythonError = e;
     }
 
     if (pythonError) {
-        Log.error('paHelper', 'getCards', pythonError);
+        Log.error('paHelper', 'execHelper', pythonError);
         if (stderr) {
-            Log.error('paHelper', 'getCards', `(${ret}) ${stderr}`);
+            Log.error('paHelper', 'execHelper', `(${ret}) ${stderr}`);
         }
     }
 
@@ -62,17 +94,17 @@ async function execHelper(index = undefined) {
     try {
         data = JSON.parse(stdout);
     } catch (e) {
-        Log.error('paHelper', 'getCards', e);
+        Log.error('paHelper', 'execHelper', e);
         return null;
     }
 
     if (!data || typeof data !== 'object') {
-        Log.error('paHelper', 'getCards', 'Invalid response');
+        Log.error('paHelper', 'execHelper', 'Invalid response');
         return null;
     }
 
     if ('success' in data && data.success === false) {
-        Log.error('paHelper', 'getCards', `Error: ${data.error}`);
+        Log.error('paHelper', 'execHelper', `Error: ${data.error}`);
         return null;
     }
 
