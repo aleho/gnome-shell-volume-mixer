cc6b86f6fc018a050c1c660f14232533260bdef5
Alexander Hofbauer <alex@derhofbauer.at>
2021-05-24 13:13:40 +0200

    Signals already provides a disconnectAll

--- shell-volume-mixer@derhofbauer.at/lib/utils/eventBroker.js
+++ shell-volume-mixer@derhofbauer.at/lib/utils/eventBroker.js
@@ -27,14 +27,6 @@ var EventBroker = class {
             callback(Log.dump(this._signalConnections));
         });
     }
-
-    disconnectAll() {
-        for (let signal of this._signalConnections) {
-            if (!signal.disconnected) {
-                this.disconnect(signal.id);
-            }
-        }
-    }
 };
 
 Signals.addSignalMethods(EventBroker.prototype);
