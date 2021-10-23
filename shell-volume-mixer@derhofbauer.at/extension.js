36c99074480d0e663b5ec661e1c5bad81b1b403f
Alexander Hofbauer <alex@derhofbauer.at>
2021-10-23 17:04:21 +0200

    Switch to required extension enabling

--- shell-volume-mixer@derhofbauer.at/extension.js
+++ shell-volume-mixer@derhofbauer.at/extension.js
@@ -3,26 +3,15 @@
  *
  * Advanced mixer extension.
  *
- * @author Harry Karvonen <harry.karvonen@gmail.com>
  * @author Alexander Hofbauer <alex@derhofbauer.at>
  */
 
-/* exported init, enable, disable */
+/* exported init */
 
 const Lib = imports.misc.extensionUtils.getCurrentExtension().imports.lib;
-
 const { Extension } = Lib.main;
 
-let extension;
 
 function init() {
-    extension = new Extension();
-}
-
-function enable() {
-    extension.enable();
-}
-
-function disable() {
-    extension.disable();
+    return new Extension();
 }
